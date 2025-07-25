770f967944d2646cc8326117c96ccd2d9d50ee68
Prakhar Gussain <prakhargussain069@gmail.com>
2025-07-25 10:43:19 +0530

    feat: Set up Express server with basic routing and auth route integration

--- server/src/routes/auth-route.js
+++ server/src/routes/auth-route.js
@@ -1,5 +1,15 @@
 import express from 'express';
 
 const router = express.Router();
+router.get('/signup',(req,res)=>{
+    res.send("hemlo");
+})
+router.get('/login',(req,res)=>{
+    res.send("hemlo");
+})
+router.get('/signin',(req,res)=>{
+    res.send("hemlo");
+})
+
 
 export default router;
