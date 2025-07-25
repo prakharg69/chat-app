11a79ddf3e28174c09aad64ad989997b7d276ff5
Prakhar Gussain <prakhargussain069@gmail.com>
2025-07-25 10:38:54 +0530

    chore: Set up initial Express router boilerplate

---
+++ server/src/server.js
@@ -0,0 +1,26 @@
+import express from "express";
+import bodyParser from "body-parser";
+import dotenv from "dotenv";
+import authRoutes from "./routes/auth-route.js"
+
+dotenv.config();
+
+const app = express();
+
+// Middleware
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+// Route
+app.get("/", (req, res) => {
+  res.send("jbaha");
+  console.log(`Request received: ${req.method} ${req.url}`);
+});
+app.use("/api/auth",authRoutes);
+
+const PORT = process.env.PORT;
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
+
+
